f56af3833f57c4b055bc5a7eb4750b39413a7109
Khvede93 <gio.khvede93@gmail.com>
2024-09-12 23:57:14 +0200

    temp structure

--- src/components/load-more-button/LoadMoreButton.jsx
+++ src/components/load-more-button/LoadMoreButton.jsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './loadMoreButton.css';
 
 export const LoadMoreButton = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [errorM, setErrorM] = useState('');
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        'https://dummyjson.com/products?limit=10&skip=10'
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error ! ${response.status}`);
+      }
+    } catch (e) {
+      setErrorM(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <div>Loading Data ! Please Wait</div>;
+  }
+
+  if (errorM !== '') {
+    return <div>{errorM}</div>;
+  }
+
   return <div className='container'>LoadMoreButton</div>;
 };
