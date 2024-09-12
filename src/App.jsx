4da95788ed3efc9ffdcd272587a953ccb630a9a8
Khvede93 <gio.khvede93@gmail.com>
2024-09-12 21:38:26 +0200

    prepare project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import './App.css';
+import { LoadMoreButton } from './components/load-more-button/LoadMoreButton';
+
+function App() {
+  return (
+    <div className='app'>
+      <LoadMoreButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/load-more-button/LoadMoreButton.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './loadMoreButton.css';
+
+export const LoadMoreButton = () => {
+  return <div className='container'>LoadMoreButton</div>;
+};
